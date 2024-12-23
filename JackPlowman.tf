08e2e1d8faacd3cb22800c650a398874fa222192
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-23 11:39:20 +0000

    Add JackPlowman Repository (#10)
    # Pull Request
    
    ## Description
    
    This pull request introduces several changes to configure and manage a
    GitHub repository using Terraform. The changes include adding a new
    provider, defining a repository resource, setting up the Terraform
    configuration, and adding a variable for the GitHub token.
    
    Key changes:
    
    ### Provider Configuration:
    * Added a new provider configuration for
    `registry.opentofu.org/integrations/github` with version `6.4.0` in the
    `.terraform.lock.hcl` file.
    
    ### Repository Resource:
    * Defined a new `github_repository` resource named `JackPlowman` in
    `JackPlowman.tf`, including settings for visibility, merge options, and
    vulnerability alerts.
    
    ### Terraform Configuration:
    * Added the main Terraform configuration in `terraform.tf`, specifying
    the required version, providers, and backend settings.
    
    ### Variables:
    * Introduced a new variable `GITHUB_TOKEN` in `variables.tf` for GitHub
    authentication.

---
+++ JackPlowman.tf
@@ -0,0 +1,17 @@
+resource "github_repository" "JackPlowman" {
+  #checkov:skip=CKV_GIT_1
+  #checkov:skip=CKV2_GIT_1
+  name        = "JackPlowman"
+  description = "My Profile"
+  visibility  = "public"
+
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  allow_update_branch    = true
+  delete_branch_on_merge = true
+
+  squash_merge_commit_message = "PR_BODY"
+  squash_merge_commit_title   = "PR_TITLE"
+  vulnerability_alerts        = true
+}
