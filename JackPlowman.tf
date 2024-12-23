3b99498c99bd50afa2892f868e36e5f18372ca25
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-23 13:09:13 +0000

    update

--- JackPlowman.tf
+++ JackPlowman.tf
@@ -6,15 +6,15 @@ resource "github_repository" "JackPlowman" {
   visibility  = "public"
 
   # Pull Request settings
-  allow_auto_merge       = true
-  allow_merge_commit     = false
-  allow_rebase_merge     = false
-  allow_update_branch    = true
-  delete_branch_on_merge = true
+  allow_auto_merge            = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_update_branch         = true
+  delete_branch_on_merge      = true
   squash_merge_commit_message = "PR_BODY"
   squash_merge_commit_title   = "PR_TITLE"
 
   # Other settings
-  has_downloads = false
-  vulnerability_alerts        = true
+  has_downloads        = false
+  vulnerability_alerts = true
 }
