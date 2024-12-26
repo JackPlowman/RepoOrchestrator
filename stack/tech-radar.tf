b99a8619ff9dc9e284c3acf1f0d76baf80f15814
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-26 11:43:04 +0000

    Fix Tech Radar Scanning Rules (#49)
    # Pull Request
    
    ## Description
    
    This pull request includes a change to the `stack/tech-radar.tf` file to
    update the default branch protection settings. The most important
    changes include adding a new status check and modifying the required
    code scanning tools.
    
    Changes to default branch protection settings:
    
    *
    [`stack/tech-radar.tf`](diffhunk://#diff-492e56ddb82f7c2f0feda77f5a318465a9a443db5775cad33ae6161905eecc50R48-R50):
    Added "SonarCloud Scan" to the list of status checks.
    *
    [`stack/tech-radar.tf`](diffhunk://#diff-492e56ddb82f7c2f0feda77f5a318465a9a443db5775cad33ae6161905eecc50R48-R50):
    Removed "CodeQL" from the list of required code scanning tools.

--- stack/tech-radar.tf
+++ stack/tech-radar.tf
@@ -45,8 +45,9 @@ module "tech-radar_default_branch_protection" {
     "Check Markdown links",
     "Dependency Review",
     "Label Pull Request",
+    "SonarCloud Scan",
   ]
-  required_code_scanning_tools = ["CodeQL", "SonarCloud", "zizmor"]
+  required_code_scanning_tools = ["SonarCloud", "zizmor"]
 
   depends_on = [github_repository.tech-radar]
 }
