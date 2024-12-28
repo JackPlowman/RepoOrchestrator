e43ce4fbd0aeb5bc5d56ad695848441f393e8ddf
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-28 10:46:39 +0000

    Add useful-commands Repository (#54)
    # Pull Request
    
    ## Description
    
    This pull request introduces a new GitHub repository resource
    configuration in the `stack/useful-commands.tf` file. The resource is
    configured with various settings for repository features, pull request
    settings, and other options.
    
    Key changes include:
    
    * Added a new `github_repository` resource named `useful-commands` with
    public visibility.
    * Enabled repository features such as issues and projects.
    * Configured pull request settings to allow auto-merge, disallow merge
    and rebase commits, enable branch updates, and delete branches on merge.
    * Set additional options such as disabling downloads, enabling
    vulnerability alerts, and requiring web commit signoff.

---
+++ stack/useful-commands.tf
@@ -0,0 +1,25 @@
+resource "github_repository" "useful-commands" {
+  #checkov:skip=CKV_GIT_1
+  #checkov:skip=CKV2_GIT_1
+  name        = "useful-commands"
+  description = ""
+  visibility  = "public"
+
+  # Repository Features
+  has_issues   = true
+  has_projects = true
+
+  # Pull Request settings
+  allow_auto_merge            = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_update_branch         = true
+  delete_branch_on_merge      = true
+  squash_merge_commit_message = "PR_BODY"
+  squash_merge_commit_title   = "PR_TITLE"
+
+  # Other settings
+  has_downloads               = false
+  vulnerability_alerts        = true
+  web_commit_signoff_required = true
+}
