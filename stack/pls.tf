0f929e46c40c91de9dbb5085f070e89d63ab4993
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-29 22:24:39 +0000

    Add pls Scanning Rules (#55)
    # Pull Request
    
    ## Description
    
    This pull request includes an important change to the `stack/pls.tf`
    file to add a new module for default branch protection. The main change
    involves setting up required status checks and code scanning tools for
    the `pls` repository.
    
    The most important change is:
    
    *
    [`stack/pls.tf`](diffhunk://#diff-0e4fa6f82344e6b675ff6ac7414659b4868df4d89e84bf2a16bf0e36ad4ccd8dR27-R45):
    Added a new `pls_default_branch_protection` module to enforce default
    branch protection rules, including required status checks and code
    scanning tools for the `pls` repository.

--- stack/pls.tf
+++ stack/pls.tf
@@ -24,3 +24,22 @@ resource "github_repository" "pls" {
   web_commit_signoff_required = true
 
 }
+
+module "pls_default_branch_protection" {
+  source = "../modules/default-branch-protection"
+
+  repository_name = github_repository.pls.name
+  required_status_checks = [
+    "Check Code Quality",
+    "Check GitHub Actions with zizmor",
+    "Check Justfile Format",
+    "Check Markdown links",
+    "Check Rust Format",
+    "Check Rust Linting",
+    "Dependency Review",
+    "Label Pull Request",
+  ]
+  required_code_scanning_tools = ["zizmor"]
+
+  depends_on = [github_repository.pls]
+}
