7e575b9bd867f0b652b7f90942bb5310bab90bf4
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-27 16:59:02 +0000

    update

---
+++ stack/repository-visualiser.tf
@@ -0,0 +1,37 @@
+resource "github_repository" "repository-visualiser" {
+  #checkov:skip=CKV_GIT_1
+  #checkov:skip=CKV2_GIT_1
+  name        = "repository-visualiser"
+  description = ""
+  visibility  = "public"
+
+  # Pull Request settings
+  allow_auto_merge            = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_update_branch         = true
+  delete_branch_on_merge      = true
+  squash_merge_commit_message = "PR_BODY"
+  squash_merge_commit_title   = "PR_TITLE"
+
+  # Other settings
+  is_template                 = true
+  has_downloads               = false
+  vulnerability_alerts        = true
+  web_commit_signoff_required = true
+
+  # Security settings
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+}
+
+resource "github_repository_dependabot_security_updates" "repository-visualiser" {
+  repository = github_repository.repository-visualiser.name
+  enabled    = true
+}
