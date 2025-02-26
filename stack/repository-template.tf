0359fa30c708ee17d58749abf6ff2e287b424e37
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-02-26 08:43:26 +0000

    Update repository-template status checks (#145)
    # Pull Request
    
    ## Description
    
    This pull request includes an update to the
    `repository-template_default_branch_protection` module in the
    `stack/repository-template.tf` file to enhance branch protection by
    adding a new required status check.
    
    Branch protection enhancement:
    
    *
    [`stack/repository-template.tf`](diffhunk://#diff-e423eafcab1924cdae12635150f2d1fbe733b23ad035d3f61dcf8ea5a1d4f4fbR46):
    Added "Check Justfile Format" to the list of required status checks.

--- stack/repository-template.tf
+++ stack/repository-template.tf
@@ -43,6 +43,7 @@ module "repository-template_default_branch_protection" {
   required_status_checks = [
     "Check Code Quality",
     "Check GitHub Actions with zizmor",
+    "Check Justfile Format",
     "Check Markdown links",
     "CodeQL Analysis",
     "Dependency Review",
