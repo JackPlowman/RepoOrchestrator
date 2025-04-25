f60b0ad6b8142fd98ff8009293375e45272e2e61
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-04-25 20:51:05 +0100

    Update useful-commands status checks (#230)
    # Pull Request
    
    ## Description
    
    This pull request makes a small change to the `stack/useful-commands.tf`
    file by removing the "Pinact Verify" workflow from the list of required
    status checks in the `useful-commands_default_branch_protection` module.

--- stack/useful-commands.tf
+++ stack/useful-commands.tf
@@ -36,7 +36,6 @@ module "useful-commands_default_branch_protection" {
     "CodeQL Analysis",
     "Dependency Review",
     "Label Pull Request",
-    "Pinact Verify",
   ]
   required_code_scanning_tools = ["CodeQL", "zizmor"]
 
