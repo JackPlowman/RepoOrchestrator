899e4c336e1a85bdfa4a0c4c3bcbc844c312937d
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-17 14:53:09 +0100

    Update windows-development-environment status checks (#313)
    # Pull Request
    
    ## Description
    
    This pull request updates the branch protection rules in the
    `windows-development-environment.tf` file to align with a new naming
    convention for status checks. The changes primarily involve prefixing
    existing status check names with categories like "Common Code Checks"
    and "Common Pull Request Tasks."
    
    ### Updates to branch protection rules:
    
    *
    [`stack/windows-development-environment.tf`](diffhunk://#diff-29ca7d5f255e2c62e205ee5ff3e9620106ad694515f8740faa831231af0dd3beL40-R46):
    Renamed several status checks under the `required_status_checks` field
    to include category prefixes, such as "Common Code Checks" and "Common
    Pull Request Tasks," for better organization and clarity.

--- stack/windows-development-environment.tf
+++ stack/windows-development-environment.tf
@@ -37,13 +37,13 @@ module "windows-development-environment_default_branch_protection" {
   repository_name = github_repository.windows-development-environment.name
   required_status_checks = [
     "Check Code Quality",
-    "Check GitHub Actions with zizmor",
-    "Check Justfile Format",
-    "Check Markdown links",
     "CodeQL Analysis",
-    "Dependency Review",
-    "Label Pull Request",
-    "Lefthook Validate",
+    "Common Code Checks / Check GitHub Actions with zizmor",
+    "Common Code Checks / Check Justfile Format",
+    "Common Code Checks / Check Markdown links",
+    "Common Code Checks / Lefthook Validate",
+    "Common Pull Request Tasks / Dependency Review",
+    "Common Pull Request Tasks / Label Pull Request",
   ]
   required_code_scanning_tools = ["CodeQL", "zizmor"]
 
