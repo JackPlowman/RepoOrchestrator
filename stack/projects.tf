29755d4acda7dc8d000aa9fa14b491430a2e9b4e
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-24 23:32:42 +0100

    Update projects status checks (#347)
    # Pull Request
    
    ## Description
    
    This pull request updates the `required_status_checks` list in the
    `projects_default_branch_protection` module to reflect a more specific
    naming convention for one of the checks.
    
    *
    [`stack/projects.tf`](diffhunk://#diff-b0a37de59757357b8080f1ff625c99785318d1e20d1679ea7caeeafc0a079774L50-R50):
    Updated the `required_status_checks` list to replace "CodeQL Analysis"
    with the more descriptive "CodeQL Analysis (actions) / Analyse code" in
    the `projects_default_branch_protection` module.

--- stack/projects.tf
+++ stack/projects.tf
@@ -47,7 +47,7 @@ module "projects_default_branch_protection" {
   repository_name = github_repository.projects.name
   required_status_checks = [
     "Check Code Quality",
-    "CodeQL Analysis",
+    "CodeQL Analysis (actions) / Analyse code",
     "Common Code Checks / Check GitHub Actions with zizmor",
     "Common Code Checks / Check Justfile Format",
     "Common Code Checks / Check Markdown links",
