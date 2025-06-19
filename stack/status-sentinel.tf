55ca77002b39713e742f98818e0aea5b0c88d2c2
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-06-19 18:26:52 +0100

    update

--- stack/status-sentinel.tf
+++ stack/status-sentinel.tf
@@ -66,7 +66,10 @@ module "status-sentinel_default_branch_protection" {
     "Common Pull Request Tasks / Dependency Review",
     "Common Pull Request Tasks / Label Pull Request",
     "Run CodeLimit",
-    "Run Python Code Checks",
+    "Run Python Tests Format Checks",
+    "Run Python Tests Lint Checks",
+    "Run Python Tests Lockfile Check",
+    "Run Python Tests Type Checks",
     "Run TypeScript Code Checks",
   ]
   required_code_scanning_tools = ["CodeQL", "zizmor", "ruff", "ESLint"]
