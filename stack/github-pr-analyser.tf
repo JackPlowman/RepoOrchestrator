d5858de20c98ffc77ce2a90013e520ef2d7ef305
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-22 14:20:37 +0100

    update

--- stack/github-pr-analyser.tf
+++ stack/github-pr-analyser.tf
@@ -43,28 +43,18 @@ module "github-pr-analyser_default_branch_protection" {
   source = "../modules/default-branch-protection"
 
   repository_name = github_repository.github-pr-analyser.name
-  required_status_checks = [
-    "Check Code Quality",
-    "Check Go Format",
-    "Check Pull Request Title",
-    "CodeQL Analysis (actions) / Analyse code",
-    "CodeQL Analysis (go) / Analyse code",
-    "Common Code Checks / Check File Formats with EditorConfig Checker",
-    "Common Code Checks / Check GitHub Actions with Actionlint",
-    "Common Code Checks / Check GitHub Actions with zizmor",
-    "Common Code Checks / Check Justfile Format",
-    "Common Code Checks / Check Markdown links",
-    "Common Code Checks / Check for Secrets with Gitleaks",
-    "Common Code Checks / Check for Secrets with TruffleHog",
-    "Common Code Checks / Check for Vulnerabilities with Grype",
-    "Common Code Checks / Lefthook Validate",
-    "Common Code Checks / Pinact Check",
-    "Common Pull Request Tasks / Dependency Review",
-    "Common Pull Request Tasks / Label Pull Request",
-    "Run Go Vulnerability Check",
-    "Run Local Action",
-    "Run Unit Tests",
-  ]
+  required_status_checks = concat(
+    [
+      "Check Go Format",
+      "Check Pull Request Title",
+      "CodeQL Analysis (actions) / Analyse code",
+      "CodeQL Analysis (go) / Analyse code",
+      "Run Go Vulnerability Check",
+      "Run Local Action",
+      "Run Unit Tests",
+    ],
+    local.common_required_status_checks
+  )
   required_code_scanning_tools = local.common_code_scanning_tools
 
   depends_on = [github_repository.github-pr-analyser]
