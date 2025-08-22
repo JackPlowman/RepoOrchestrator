d387177306e0c5b2d949f0b456946bda44b34171
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-22 12:09:51 +0100

    update

---
+++ stack/coding-metrics.tf
@@ -0,0 +1,41 @@
+resource "github_repository" "coding-metrics" {
+  #checkov:skip=CKV_GIT_1
+  #checkov:skip=CKV2_GIT_1
+  name        = "coding-metrics"
+  description = ""
+  visibility  = "public"
+
+  # Repository Features
+  has_issues   = true
+  has_projects = false
+
+  # Pull Request settings
+  allow_auto_merge            = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_update_branch         = true
+  delete_branch_on_merge      = true
+  squash_merge_commit_message = "PR_BODY"
+  squash_merge_commit_title   = "PR_TITLE"
+
+  # Other settings
+  has_downloads               = false
+  vulnerability_alerts        = true
+  web_commit_signoff_required = true
+
+  # Security settings
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+
+  template {
+    include_all_branches = false
+    owner                = "JackPlowman"
+    repository           = "repository-template"
+  }
+}
