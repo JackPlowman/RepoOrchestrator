0f1f2320fd6bd3ec509a89a5cb77f5cfc477ce50
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-28 21:49:47 +0100

    update

--- stack/repository-template.tf
+++ stack/repository-template.tf
@@ -41,9 +41,7 @@ module "repository-template_default_branch_protection" {
 
   repository_name = github_repository.repository-template.name
   required_status_checks = concat(
-    [
-      "CodeQL Analysis (actions) / Analyse code",
-    ],
+    ["CodeQL Analysis (actions) / Analyse code"],
     local.common_required_status_checks
   )
   required_code_scanning_tools = local.common_code_scanning_tools
