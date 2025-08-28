739e69eac61ddef9e5f50dd9cef3104f364d1622
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-28 22:10:10 +0100

    update

--- stack/tech-radar.tf
+++ stack/tech-radar.tf
@@ -53,26 +53,17 @@ module "tech-radar_default_branch_protection" {
   source = "../modules/default-branch-protection"
 
   repository_name = github_repository.tech-radar.name
-  required_status_checks = [
-    "CodeQL Analysis (actions) / Analyse code",
-    "Common Code Checks / Check File Formats with EditorConfig Checker",
-    "Common Code Checks / Check GitHub Actions with Actionlint",
-    "Common Code Checks / Check GitHub Actions with zizmor",
-    "Common Code Checks / Check Justfile Format",
-    "Common Code Checks / Check Markdown links",
-    "Common Code Checks / Check for Secrets with Gitleaks",
-    "Common Code Checks / Check for Secrets with TruffleHog",
-    "Common Code Checks / Check for Vulnerabilities with Grype",
-    "Common Code Checks / Lefthook Validate",
-    "Common Code Checks / Pinact Check",
-    "Common Pull Request Tasks / Dependency Review",
-    "Common Pull Request Tasks / Label Pull Request",
-    "Run Python Tests Format Checks",
-    "Run Python Tests Lint Checks",
-    "Run Python Tests Lockfile Check",
-    "Run Python Tests Type Checks",
-    "SonarCloud Scan",
-  ]
+  required_status_checks = concat(
+    [
+      "CodeQL Analysis (actions) / Analyse code",
+      "Run Python Tests Format Checks",
+      "Run Python Tests Lint Checks",
+      "Run Python Tests Lockfile Check",
+      "Run Python Tests Type Checks",
+      "SonarCloud Scan",
+    ],
+    local.common_required_status_checks
+  )
   required_code_scanning_tools = concat(local.common_code_scanning_tools, ["Ruff", "SonarCloud"])
 
   depends_on = [github_repository.tech-radar]
